796e466923dfb39d4143433e438c3adafe9365ae
pietari koskinen <pkoskinen1@users.noreply.github.com>
2016-05-12 11:00:26 +0300

    added the design for login page

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title> To Do List </title>
+    <title> Log in page </title>
 
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<?php require_once("header.php"); ?>
 <?php
 
 if (isset($_POST["login"])){
@@ -41,18 +42,18 @@ if (isset($_POST["login"])){
 <form class="form-horizontal">
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">username</label>
-    <div class="col-sm-10">
+    <div class="col-sm-5">
       <input type="text" class="form-control" id="inputEmail3" placeholder="username">
     </div>
   </div>
   <div class="form-group">
     <label for="inputPassword3" class="col-sm-2 control-label">password</label>
-    <div class="col-sm-10">
+    <div class="col-sm-5">
       <input type="password" class="form-control" id="inputPassword3" placeholder="password">
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
+    <div class="col-sm-offset-2 col-sm-5">
       <div class="checkbox">
         <label>
           <input type="checkbox"> Remember me
@@ -61,16 +62,38 @@ if (isset($_POST["login"])){
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
+    <div class="col-sm-offset-2 col-sm-5">
       <button type="submit" class="btn btn-default">Sign in</button>
     </div>
   </div>
 </form>
-
-		<input type="text" placeholder="username" name="username"><br></br>
-		<input type="password" placeholder="password" name="password"><br></br>
-		<input type="text" placeholder="Full Name" name="Full_Name"><br></br>
 	
-		<input type="submit" name="signup" value="Sign Up">
+<h1> Sign Up </h1>
 
+	<form class="form-horizontal">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">username</label>
+    <div class="col-sm-5">
+      <input type="text" class="form-control" id="inputEmail3" placeholder="username">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">password</label>
+    <div class="col-sm-5">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="password">
+    </div>
+  </div>
+<form class="form-horizontal">
+	<div class="form-group">
+		<label for="inputName" class="col-sm-2 control-label">Full Name</label>
+		<div class="col-sm-5">
+	<input type="text" placeholder="Full Name" name="Full_Name">
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-5">
+      <button type="submit" class="btn btn-default">Sign up</button>
+    </div>
+  </div>
 </form>
